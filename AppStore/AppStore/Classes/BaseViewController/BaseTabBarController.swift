84534756021b4d99e903bd59b6e62af94af984a6
醉看红尘这场梦 <15914115336@163.com>
2023-03-01 19:59:24 +0800

    去掉网络接口失效

--- AppStore/AppStore/Classes/BaseViewController/BaseTabBarController.swift
+++ AppStore/AppStore/Classes/BaseViewController/BaseTabBarController.swift
@@ -17,8 +17,7 @@ class BaseTabBarController: UITabBarController {
             createNavController(viewController: TodayController(), title: "Today", imageName: "today_icon"),
             createNavController(viewController: AppsPageController(), title: "Apps", imageName: "apps"),
             createNavController(viewController: AppsSearchController(), title: "Search", imageName: "search"),
-            createNavController(viewController: MusicController(), title: "Muisc", imageName: "music"),
-            createNavController(viewController: TestViewController(), title: "test", imageName: "music")
+            createNavController(viewController: MusicController(), title: "Muisc", imageName: "music")
         ]
     }
     
